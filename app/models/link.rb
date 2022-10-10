7e6b2c1023a1e0cf19326cd2c9a78cd3cfb09527
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2022-10-10 17:11:28 -0400

    Add a way to sync links

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,27 @@
 class Link < ApplicationRecord
+
+  class << self
+    def find_urls(sitemap)
+      sitemap.to_a.select {|x| x.end_with? ".html" }.reject {|x| x.include?("page") || x.include?("/tags/") || x.include?("author") }
+    end
+
+    def sync!(sitemap)
+      find_urls(sitemap).each do |str|
+        puts "Url: #{str}"
+
+        link = Link.find_or_create_by(url: str)
+
+        mechanize = Mechanize.new
+        
+        page = mechanize.get(str)
+
+        description = page.at('meta[name="description"]')[:content]
+        
+        link.update! title: page.title, description: description
+
+        puts page.title
+      end
+    end
+  end
+  
 end
--- lib/tasks/sitemap-consumer.rake
+++ lib/tasks/sitemap-consumer.rake
@@ -1,13 +1,10 @@
-task :read do
+task read: :environment do
   require "sitemap-parser"
   puts "Reading sitemap..."
 
   sitemap = SitemapParser.new "http://ombulabs.com.s3.amazonaws.com/sitemap.xml", recurse: true
 
-  sitemap.urls # => Array of Nokigiri XML::Node objects
-  sitemap.to_a.select {|x| x.end_with? ".html" }.reject {|x| x.include?("page") || x.include?("/tags/") || x.include?("author") }.each do |str|
-    puts "Url: #{str}"
-  end
+  Link.sync!(sitemap)
 
   puts "Read sitemap!"
 end
