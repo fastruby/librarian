fdd5ef32e49b7bd7eb789233b58f94a0bfba64a5
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2022-10-11 10:58:28 -0400

    Add shares to the set of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :links
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :links do
+    resources :shares
+  end
 end
