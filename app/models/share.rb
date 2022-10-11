4e12531778b59229d20a938c1e2ebc2602f9d3be
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2022-10-11 13:53:19 -0400

    Implement logic to shorten shared link

--- app/models/share.rb
+++ app/models/share.rb
@@ -3,23 +3,28 @@ class Share < ApplicationRecord
 
   validates :utm_source, :utm_campaign, :utm_medium, :utm_term, presence: true
 
-  def utm_params
-    { 
-      "utm_source" => utm_source,  
-      "utm_medium" => utm_medium,  
-      "utm_campaign" => utm_campaign,  
-      "utm_term" => utm_term,  
-      "utm_content" => utm_content,  
-      "utm_id" => utm_id
-    }
-  end
-
   def calculated_url
     uri = URI(link.url)
 
     params = Hash[URI.decode_www_form(uri.query || '')].merge(utm_params)
     uri.query = URI.encode_www_form(params)
-    
+
     uri.to_s
+  end
+
+  def shorten
+    rebrandly_client = OmbuLabs::Shortener::RebrandlyClient.new(calculated_url)
+    rebrandly_client.shorten
+  end
+
+  def utm_params
+    {
+      "utm_source" => utm_source,
+      "utm_medium" => utm_medium,
+      "utm_campaign" => utm_campaign,
+      "utm_term" => utm_term,
+      "utm_content" => utm_content,
+      "utm_id" => utm_id
+    }
   end
 end
