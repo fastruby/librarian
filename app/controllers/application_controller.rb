9477807eecced35a9b2189e96004c67214658540
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2022-10-22 22:52:32 -0400

    Authenticate with OmniAuth + GitHub + OmbuLabs org

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with(
-    name: ENV["ADMIN_USERNAME"],
-    password: ENV["ADMIN_PASSWORD"]
-  )
+  before_action :authenticate_user!
 end
