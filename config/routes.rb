7d8152c0cf41c348515aa80766c0ef01686cb66b
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2022-10-22 22:52:56 -0400

    Mount OmbuLabs::Auth::Engine as instructed by ombu_labs-auth gem README

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount OmbuLabs::Auth::Engine, at: '/', as: 'ombu_labs_auth'
+
   root to: "links#index"
   resources :links, only: [:index, :show] do
     resources :shares
