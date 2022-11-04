e74c4e5bf184e438da64c060869365b406b2c0f7
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2022-11-04 14:39:24 -0400

    Add published_at as a new column for links

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_26_141851) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_04_183204) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_26_141851) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "open_graph_description", default: ""
+    t.datetime "published_at", precision: nil
     t.index ["url"], name: "index_links_on_url", opclass: :gin_trgm_ops, using: :gin
   end
 
@@ -38,6 +39,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_26_141851) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["link_id"], name: "index_shares_on_link_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "shares", "links"
