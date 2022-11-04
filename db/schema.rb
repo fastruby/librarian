6e3ab5177aa90e9db99e26ec04fb550f35857f7d
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2022-11-04 15:48:48 -0400

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -41,17 +41,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_04_183204) do
     t.index ["link_id"], name: "index_shares_on_link_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
   add_foreign_key "shares", "links"
 end
