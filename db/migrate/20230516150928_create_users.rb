f09cc72dba26925a53429d89234df41aece9f201
Ariel Juodziukynas <arieljuod@gmail.com>
2023-06-16 16:54:19 -0300

    Fix the last migration to actually create the users table

---
+++ db/migrate/20230516150928_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table "users", force: :cascade do |t|
+      t.string "email", default: "", null: false
+      t.string "encrypted_password", default: "", null: false
+      t.string "reset_password_token"
+      t.datetime "reset_password_sent_at"
+      t.datetime "remember_created_at"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.string "provider"
+      t.string "uid"
+      t.string "name"
+      t.index ["email"], name: "index_users_on_email", unique: true
+      t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    end
+  end
+end
