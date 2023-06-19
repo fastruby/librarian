d8f20216042627b1338a0a8327464906202b16a0
Ariel Juodziukynas <arieljuod@gmail.com>
2023-06-18 21:30:46 -0300

    Fix the last migration to actually create the users table (#20)

--- db/migrate/20230516150928_add_auth_columns_to_user.rb
+++
@@ -1,9 +0,0 @@
-class AddAuthColumnsToUser < ActiveRecord::Migration[7.0]
-  def change
-    change_table :users do |t|
-      t.string :provider
-      t.string :uid, unique: true
-      t.string :name
-    end
-  end
-end
