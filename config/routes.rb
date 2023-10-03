6743fbf8c7ba3ee58e31ebb6b2adf1fbf38a3dfc
Henrique Medeiros <hmdros@gmail.com>
2023-10-03 10:20:07 -0300

    Librarian-22 - Add button_to execute :read task
    Using redirect_back_or_to to redirect after read task completed
    successfully.
    
    See: https://github.com/fastruby/librarian/issues/22

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :shares
     get '/shares/:id/clone', to: 'shares#clone', as: "clone"
   end
+  post 'execute_read_task', to: 'application#execute_read_task'
 
   mount OmbuLabs::Auth::Engine, at: '/', as: 'ombu_labs_auth'
 end
